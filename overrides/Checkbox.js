ea251d62e2147feeeddb38acd468f1ecd5389dd0
Bryan P Johnson <bryan@12ftguru.com>
2012-06-18 16:42:50 -0400

    fixes for loading radios

--- overrides/Checkbox.js
+++ overrides/Checkbox.js
@@ -93,5 +93,42 @@ Ext.define('Ext.overrides.field.Radio', {
             field = fields[i];
             field.onChange();
         }
+    },
+    /**
+     * Set the matched radio field's status (that has the same value as the given string) to checked
+     * @param {String} value The value of the radio field to check
+     * @return {Ext.field.Radio} The field that is checked
+     */
+    setGroupValue: function(value) {
+        var fields = this.getSameGroupFields(),
+            ln = fields.length,
+            i = 0,
+            field;
+        for (; i < ln; i++) {
+            field = fields[i];
+            if (field._value === value) {
+                field.setChecked(true);
+                return field;
+            }
+        }
+    },
+    getSameGroupFields: function() {
+        var component = this.up('formpanel') || this.up('fieldset'),
+            name = this.getName(),
+            replaceLeft = this.qsaLeftRe,
+            replaceRight = this.qsaRightRe,
+            components = [],
+            elements, element, i, ln;
+
+        if (!component) {
+            // <debug>
+            Ext.Logger.warn('Ext.field.Radio components must always be descendants of an Ext.form.Panel or Ext.form.FieldSet.');
+            // </debug>
+            component = Ext.Viewport;
+        }
+
+        elements = component.query('[name=' + name + ']');
+
+        return elements;
     }
 });
